7fda48748db22bfb322e5051d2bdc43a3b7656c5
Beknazar <68378988+Beknasar@users.noreply.github.com>
2024-02-25 04:15:34 -1200

    Add files via upload

---
+++ prime_number_checker.py
@@ -0,0 +1,27 @@
+#Write your code below this line 👇
+def prime_checker(number):
+  is_prime = True
+  #number = 5
+  #5 / 2 = 
+  #5 / 3 =
+  #5 / 4 = 
+  for i in range(2, number):
+    if number % i == 0:
+      is_prime = False
+
+  if is_prime:
+    print("It's a prime number.")
+  else:
+    print("It's not a prime number.")
+
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
+
